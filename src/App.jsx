4624914fa03cf12cc83133170fed21de3a284bbd
Nouh1408 <ahmedinouh@gmail.com>
2025-07-21 14:26:16 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Home from "./Components/Home/Home"
+import About from "./Components/About/About"
+import React from "react";
+import Btn from "./Components/Btn/Btn";
+
+
+
+function App() {
+  
+
+return (
+ <React.Fragment>
+     <h2 className="bg-danger">Ahmed </h2>
+     <i class="fa-solid fa-house"></i>
+ </React.Fragment>
+  )
+}
+
+export default App 
---
+++ src/Components/Btn/Btn.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Btn() {
+  return (
+    <div>
+      <button type="button" className="btn btn-outline-primary">Primary</button>
+
+    </div>
+  )
+}
