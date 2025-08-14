db072983a022d5fbff9a979abbb682a17baff5c1
Nouh1408 <ahmedinouh@gmail.com>
2025-08-14 15:30:26 +0300

    img

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import img from '../../assets/img.png'
 
 export default function Error() {
   return (
     <div>
       <div className="container">
-        <div className='d-flex justify-content-center'>
+        <div className='d-flex justify-content-center flex-column'>
             <h1>404 Page not Found</h1>
+            {/* <img src={img} alt="" /> */}
+            <img src="img.png" alt="" />
         </div>
       </div>
     </div>
