bf58bae4109431a55f556d758e42da6836ee954e
Nouh1408 <ahmedinouh@gmail.com>
2025-08-15 10:57:06 +0300

    loading complete

--- src/Components/Posts/posts.jsx
+++ src/Components/Posts/posts.jsx
@@ -3,21 +3,24 @@ import React, { useEffect, useState } from 'react'
 export default function posts() {
 
     const [posts,setPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(()=>{
         getData()
     },[])
 
     async function getData(){
+      setIsLoading(true)
         const response = await fetch("https://jsonplaceholder.typicode.com/posts")
         const data = await response.json()
+        setIsLoading(false)
         setPosts(data)
     }
   return (
     <div>
         <h1>Posts Page</h1>
       {
-        posts.map((p)=><h2>{p.id}</h2>)
+       isLoading? <p>loading...</p>: posts.map((p)=><h2>{p.id}</h2>)
       }
     </div>
   )
