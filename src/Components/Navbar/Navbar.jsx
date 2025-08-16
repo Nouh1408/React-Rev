2b035a31542790940664c0cf4f176b70dfff5acc
Nouh1408 <ahmedinouh@gmail.com>
2025-08-16 18:30:19 +0300

    Navbar with flowbite

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,35 +5,32 @@ import { Link, NavLink } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
-      <nav  className="navbar navbar-expand-lg  top-0">
-  <div className="container">
-    <Link className="navbar-brand" to="/">Logo</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link " aria-current="page" to="/">Home</NavLink>
-        </li>
      
-        <li className="nav-item">
-          <NavLink className="nav-link "  to="/about">About</NavLink>
+
+<nav class="bg-white border-gray-200 dark:bg-red-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <NavLink to={'/'} class="flex items-center space-x-3 rtl:space-x-reverse">
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tailwind</span>
+    </NavLink>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+        <li>
+          <NavLink to={'/'} class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 " aria-current="page">Home</NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink className="nav-link " to="/contact">Contact</NavLink>
+        <li>
+          <NavLink to={'about'} class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink className="nav-link " to="/posts">posts</NavLink>
+        <li>
+          <NavLink to={'contact'} class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</NavLink>
+        </li>
+        <li>
+          <NavLink to={'posts'} class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</NavLink>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
+
 
     </div>
   )
